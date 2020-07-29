93d0a77be6c25e0a789329d18085ab6323a0ad51
Csaba Tóth <tothcs1105@gmail.com>
2020-07-29 23:54:56 +0000

    Refactor in connector

--- rangefinder/connector.py
+++ rangefinder/connector.py
@@ -12,8 +12,8 @@ def connect(bluetooth_address, port):
 def find(dev_bluetooth_name):
     print('Searching for ' + dev_bluetooth_name)
     nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
-    for index, val in enumerate(nearby_devices):
-        addr, name = val
+    for item in enumerate(nearby_devices):
+        addr, name = item[1]
         if dev_bluetooth_name in name:
             print('Found ' + name + ' @', addr)
-            return val
+            return item[1]

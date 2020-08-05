224b99eb65866e96d03e5750b2fce698ece6424f
Csaba Tóth <tothcs1105@gmail.com>
2020-08-05 05:53:22 +0000

    Dispatcher class added

---
+++ rangefinder/connection/dispatcher.py
@@ -0,0 +1,12 @@
+import bluetooth
+
+class Dispatcher:
+    def __init__(self, socket: bluetooth.BluetoothSocket):
+        self.__socket = socket
+
+    @property
+    def is_connected(self) -> bool:
+        return self.__socket.connected
+
+    def send_Request(self, request):
+        self.__socket.send(request)
--- rangefinder/rangefinder.py
+++ rangefinder/rangefinder.py
@@ -1,3 +1,12 @@
+from connection.dispatcher import Dispatcher
 
 class RangeFinder:
+    
+    def __init__(self, dispatcher: Dispatcher):
+        self._dispatcher = dispatcher
+
+    @property
+    def isConnected(self) -> bool:
+        return self._dispatcher.isConnected()
+
     

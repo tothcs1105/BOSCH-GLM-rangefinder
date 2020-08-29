9f47bf00f68dee70b0b13941e8ceeb6740be3180
Csaba Tóth <tothcs1105@gmail.com>
2020-08-29 23:41:40 +0000

    Linting errors fix in tests

--- tests/connector_test.py
+++ tests/connector_test.py
@@ -4,6 +4,7 @@ from unittest.mock import call, Mock
 import bluetooth
 from rangefinder.connection import connector
 
+
 class TestConnector(unittest.TestCase):
 
     def test_connect(self):
@@ -11,25 +12,31 @@ class TestConnector(unittest.TestCase):
         port = 1010
 
         socket = Mock()
-        bluetooth.BluetoothSocket = Mock(return_value = socket)
+        bluetooth.BluetoothSocket = Mock(return_value=socket)
 
         result = connector.connect(address, port)
 
         self.assertEqual(result, socket)
-        self.assertEqual(bluetooth.BluetoothSocket.mock_calls, [call(bluetooth.RFCOMM)])
+        self.assertEqual(
+            bluetooth.BluetoothSocket.mock_calls,
+            [call(bluetooth.RFCOMM)])
         self.assertEqual(socket.mock_calls, [call.connect((address, port))])
 
     def test_find(self):
         dev_bluetooth_name = 'test_bl_name'
-        nearby_devices = [('addr1','test1'), ('addr2','test2'), ('addr3','test_bl_name')]
+        nearby_devices = [
+            ('addr1', 'test1'),
+            ('addr2', 'test2'),
+            ('addr3', 'test_bl_name')]
 
-        bluetooth.discover_devices = Mock(return_value = nearby_devices)
+        bluetooth.discover_devices = Mock(return_value=nearby_devices)
 
         result = connector.find(dev_bluetooth_name)
-        
+
         self.assertIsNotNone(result)
         self.assertEqual(result.addr, "addr3")
         self.assertEqual(result.name, "test_bl_name")
+
 
 if __name__ == '__main__':
     unittest.main()
